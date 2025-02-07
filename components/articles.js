c7abe543098d5dafc6e52fc71be6d80f76d4b489
Sergio Marin <higher.vnf@gmail.com>
2025-02-07 14:41:54 -0300

    add new styles to the blog nothing to fancy

--- components/articles.js
+++ components/articles.js
@@ -3,21 +3,21 @@ import { getLatestArticles } from "../js/utils.js";
 window.addEventListener("WebComponentsReady", function () {
 	var Component = {
 		articles: [],
-        err: null,
+		err: null,
 		connectedCallback() {
-            console.log("articles loading")
+			console.log("articles loading");
 			getLatestArticles()
 				.then((response) => {
-					this.articles = response
-                    this.updateComponent();
-                })
-                .catch(err => {
-                    this.err = err
-                })
+					this.articles = response;
+					this.updateComponent();
+				})
+				.catch((err) => {
+					this.err = err;
+				});
 		},
 		render() {
 			return `
-            <section class="articles-outer">
+			<section class="articles-outer">
                 <section class="articles">
                     <header>
                         <h4>Latest articles on Medium</h4>
@@ -25,19 +25,18 @@ window.addEventListener("WebComponentsReady", function () {
                     ${this.articles.map(this.article).join("")}
                 </section>
             </section>
-        
       `;
 		},
-        article(article = {}, _) {
-            return `
-            <section class="article">
+		article(article = {}, _) {
+			return `
+			<section class="article">
                 <header>
                     <h5><a href="${article.link}" target="_blank">${article.title}</a></h5>
                 </header>
-                <p>Publish on: ${article.pubDate.split(' ')[0]}</p>
-            </article>
-            `
-        }
+                <p class="publish-date"><span class="publish-tag">Published on:</span> ${article.pubDate.split(" ")[0]}</p>
+            </section>
+            `;
+		},
 	};
 	import("../components/component-builder.js").then(function () {
 		CreateComponent("latest-articles", Component);
